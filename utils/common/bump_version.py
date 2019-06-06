7d00a4d853d5f31e26aab814940ab4353c962620
Massimo Luraschi <massimo.luraschi-extern@renault.com>
2019-06-06 11:48:39 +0200

    Testing bump in Travis

--- utils/common/bump_version.py
+++ utils/common/bump_version.py
@@ -11,6 +11,8 @@ class MergeRequestIDNotFoundException(Exception):
 
 
 COMMIT_MESSAGE = "Auto-bumping project to version {}"
+TRAVIS_EMAIL = "build@travis-ci.com"
+TRAVIS_NAME = "Travis CI"
 
 
 def git(*args):
@@ -55,53 +57,57 @@ def retrieve_labels_from_merge_request(merge_request_id):
 
 def bump():
     # get last commit message, it will be used later
-    message = git("log", "-1", "--pretty=%B").decode("utf-8")
+    # message = git("log", "-1", "--pretty=%B").decode("utf-8")
 
     # check out master branch, because we'll need to commit to it
-    git('checkout', 'master')
+    # git('checkout', 'master')
+    #
+    # try:
+    #     merge_request_id = extract_merge_request_id_from_commit(message)
+    # except MergeRequestIDNotFoundException as mridnf:
+    #     print(mridnf)
+    #     labels = []
+    # else:
+    #     labels = retrieve_labels_from_merge_request(merge_request_id)
+    #
+    # if "bump-minor" in labels:
+    #     bump_part = 'minor'
+    # elif "bump-major" in labels:
+    #     bump_part = 'major'
+    # else:
+    #     bump_part = 'patch'
 
-    try:
-        merge_request_id = extract_merge_request_id_from_commit(message)
-    except MergeRequestIDNotFoundException as mridnf:
-        print(mridnf)
-        labels = []
-    else:
-        labels = retrieve_labels_from_merge_request(merge_request_id)
-
-    if "bump-minor" in labels:
-        bump_part = 'minor'
-    elif "bump-major" in labels:
-        bump_part = 'major'
-    else:
-        bump_part = 'patch'
-
-    subprocess.check_output(["bumpversion", bump_part])
+    subprocess.check_output(["bumpversion", 'patch'])
 
     with open('VERSION') as f:
         version = f.readline()
 
     print(COMMIT_MESSAGE.format(version))
 
-    # commit files changed by version bumping and make the commit clean by amending previous one
-    git("commit", "-a", "-m", COMMIT_MESSAGE.format(version))
-    # push amended commit with the option to skip the CI or it will trigger same job that called this script!
-    # '--force' option is needed because previous commit has been amended
-    git("push", "--force", "-o", "ci.skip")
+    # # commit files changed by version bumping and make the commit clean by amending previous one
+    # git("commit", "-a", "-m", COMMIT_MESSAGE.format(version))
+    # # push amended commit with the option to skip the CI or it will trigger same job that called this script!
+    # # '--force' option is needed because previous commit has been amended
+    # git("push", "--force", "-o", "ci.skip")
 
     return version
 
 
-def tag_repo():
-    repository_url = os.environ["CI_REPOSITORY_URL"]
-    username = os.environ["NPA_USERNAME"]
-    password = os.environ["NPA_PASSWORD"]
-    email = os.environ["NPA_EMAIL"]
-    name = os.environ["NPA_NAME"]
+def tag_repo(travis_ci=True):
+    email = TRAVIS_EMAIL
+    name = TRAVIS_EMAIL
+    if not travis_ci:
+        repository_url = os.environ["CI_REPOSITORY_URL"]
+        username = os.environ["NPA_USERNAME"]
+        password = os.environ["NPA_PASSWORD"]
+        email = os.environ["NPA_EMAIL"]
+        name = os.environ["NPA_NAME"]
 
-    push_url = re.sub(r'([a-z]+://)[^@]*(@.*)', r'\g<1>{}:{}\g<2>'.format(username, password), repository_url)
+        push_url = re.sub(r'([a-z]+://)[^@]*(@.*)', r'\g<1>{}:{}\g<2>'.format(username, password), repository_url)
 
-    # update repo URL
-    git("remote", "set-url", "--push", "origin", push_url)
+        # update repo URL
+        git("remote", "set-url", "--push", "origin", push_url)
+
     # configure git identity
     git("config", "user.email", email)
     git("config", "user.name", name)
@@ -109,8 +115,9 @@ def tag_repo():
     tag = bump()
     print("Creating tag number", tag)
     # create a tag from new version and push it
-    git("tag", tag)
-    git("push", "origin", tag)
+    git("tag", tag, '-a', '-m', COMMIT_MESSAGE.format(tag))
+    git("push", "origin", "$TRAVIS_BRANCH", "2>&1")
+    git("push", "origin", "$TRAVIS_BRANCH", "--tags", "2>&1")
 
 
 def main():
