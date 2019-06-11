d88b15b4d327dde3191890eb857057ac609419cf
Travis CI User <travis@example.org>
2019-06-11 10:26:15 +0000

    Auto-bumping project to version 1.1.1 [skip ci]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='scarfaceiii-utils',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     package_data={
         '': ['*.json', '*.xml']
